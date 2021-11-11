52be041d2bc62c0f8651aa61d98b8e0e10f7334f
Arman <arman@nexgencommunication.net>
2021-11-11 17:16:34 +0600

    login ,register, set-permission ,get-permission-list, assign-permission ,update-password API completed

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Validator;
 use App\Models\User;
+use App\Models\Permission;
+use App\Models\UserPermission;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\RateLimiter\RequestRateLimiterInterface;
 
@@ -13,21 +15,25 @@ class AuthController extends Controller
     public function register(Request $request)
     {
 
-  
+
         $validator = Validator::make($request->all(), [
             'name' => "required",
+            "phone" => "required",
+            "role" => "required",
             "email" => "required|email",
             "password" => "required"
 
         ]);
 
         if ($validator->fails()) {
-            return response()->json(["status_code" => 400, "message" => "bad Request"]);
+            return response()->json(["status_code" => 400, "error" => $validator->errors()]);
         }
 
         $user = new User();
         $user->name = $request->name;
         $user->email = $request->email;
+        $user->phone = $request->phone;
+        $user->role = $request->role;
         $user->password = bcrypt($request->password);
         $user->Save();
 
@@ -62,18 +68,36 @@ class AuthController extends Controller
         }
 
         $user = User::where('email', $request->email)->first();
+
         $tokenResult = $user->createToken('authToken')->plainTextToken;
+
+
+        $permisson = UserPermission::leftJoin('permissions', 'permissions.id', '=', 'user_permissions.permission_id')
+            ->select('user_permissions.permission_id', "permissions.title")
+            ->where('user_permissions.user_id', Auth::id())
+            ->get();
+
+        $data = [
+            "user_id" => Auth::id(),
+            "email" => Auth::user()->email,
+            "role" => Auth::user()->role ,
+            "name" => Auth::user()->name ,
+            "phone" => Auth::user()->phone,
+            "permissions" => $permisson
+
+        ];
 
         return response()->json([
             'status_code' => 200,
-            'token' => $tokenResult
+            'data' =>   $data,
+            "token" => $tokenResult
 
         ]);
     }
 
     public function logout(Request $request)
     {
-       
+
         $request->user()->currentAccessToken()->delete();
 
         return response()->json([
@@ -81,5 +105,137 @@ class AuthController extends Controller
             'message' => "Token Deleted successfully"
 
         ]);
+    }
+
+
+    public function getPermissionList()
+    {
+
+        if (Auth::user()->role == "admin") {
+
+            $permissionList =  Permission::all();
+
+            if ($permissionList) {
+                return response()->json([
+                    'status_code' => 200,
+                    'message' => "Success",
+                    'data' => $permissionList
+
+                ]);
+            } else {
+                return response()->json([
+                    'status_code' => 200,
+                    'message' => "Empty Table",
+                    'data' => []
+
+                ]);
+            }
+        } else {
+            return response()->json(["status_code" => 400, "message" => "This user is not authorized to access this route"]);
+        }
+    }
+
+
+    public function setPermission(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            "title" => "required",
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(["status_code" => 400, "errors" => $validator->errors()]);
+        }
+
+        if (Auth::user()->role == "admin") {
+
+            $permission = new Permission();
+            $permission->title = $request->title;
+            $permission->status = 1;
+            $permission->Save();
+
+            return response()->json([
+                'status_code' => 200,
+                'message' => "Permission created successfully"
+
+            ]);
+        } else {
+            return response()->json(["status_code" => 400, "message" => "This user is not authorized to access this route"]);
+        }
+    }
+
+
+
+    public function assignPermission(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            "user_id" => "required",
+            "permission_id" => "required"
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(["status_code" => 400, "errors" => $validator->errors()]);
+        }
+
+        if (Auth::user()->role == "admin") {
+
+            $user_permission = new  UserPermission();
+            $user_permission->user_id = $request->user_id;
+            $user_permission->permission_id = $request->permission_id;
+            $user_permission->Save();
+
+            return response()->json([
+                'status_code' => 200,
+                'message' => "Permission assigned successfully"
+            ]);
+        } else {
+            return response()->json(["status_code" => 400, "message" => "This user is not authorized to access this route"]);
+        }
+    }
+
+
+    public function updatePassword(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            "old_password" => "required",
+            "new_password" => "required",
+            "confirm_password" => "required"
+
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(["status_code" => 400, "errors" => $validator->errors()]);
+        }
+
+
+
+        $hashedPassword = Auth::user()->password;
+
+        if (\Hash::check($request->old_password, $hashedPassword && $request->confirm_password == $request->new_password)) {
+
+            if (!\Hash::check($request->confirm_password, $hashedPassword)) {
+
+                $users = User::find(Auth::id());
+                $users->password = bcrypt($request->confirm_password);
+                $users->Save();
+
+                return response()->json([
+                    'status_code' => 200,
+                    'message' => "Password updated successfully"
+                ]);
+            } else {
+
+                return response()->json([
+                    'status_code' => 400,
+                    'message' => "new password can not be the old password!"
+                ]);
+            }
+        } else {
+            return response()->json([
+                'status_code' => 400,
+                'message' => "Password not matched"
+            ]);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,11 +18,15 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::post('/register',[AuthController::class,'register']);
-Route::post('/login',[AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 
-Route::group(['middleware' => ['auth:sanctum']], function(){
+Route::group(['middleware' => ['auth:sanctum']], function () {
 
-    Route::post('/logout',[AuthController::class, 'logout']);
+    Route::post('/set-permission', [AuthController::class, 'setPermission']);
+    Route::get('get-permission-list',[AuthController::class, 'getPermissionList']);
+    Route::post('/assign-permission',[AuthController::class, 'assignPermission']);
+    Route::post('/update-password',[AuthController::class, 'updatePassword']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
